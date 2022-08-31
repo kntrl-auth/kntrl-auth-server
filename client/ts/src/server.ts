8596e3564c80144344fd5de5f3b838edf5c39fb7
Dmitry Barannik <dmitryb.dev@gmail.com>
2022-08-31 19:14:09 +0300

    Preparing for publishing

---
+++ client/ts/src/server.ts
@@ -0,0 +1,34 @@
+import { AppCfg, HealthRes, HealthStatus, ServerApi } from '../generated';
+import { handleErr } from './utils';
+import { Session } from './session';
+
+export class ServerSvc {
+    private readonly api: ServerApi;
+    constructor(private readonly session: Session) {
+        this.api = new ServerApi(session._serverCfg());
+    }
+
+    async health(key?: string): Promise<HealthRes> {
+        try {
+            return (await this.api.health(key, await this.session._authenticatedAxiosCfg())).data;
+        } catch (err) {
+            if (err.reponse?.data) {
+                throw err.response?.data;
+            }
+
+            const status = {
+                status: HealthStatus.Err,
+                msg: err.message,
+            };
+            return {
+                db: { user: status, session: status, rateLimiter: status },
+                auth: {},
+                ...status,
+            };
+        }
+    }
+
+    async cfg(): Promise<AppCfg> {
+        return handleErr(this.api.cfg(await this.session._authenticatedAxiosCfg()), this.session);
+    }
+}
