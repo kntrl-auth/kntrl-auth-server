d19bbf0bb4665ced0f6fdc2b535779520db47197
Dmitry Barannik <dmitryb.dev@gmail.com>
2022-10-19 00:32:08 +0300

    Improved sh examples

--- examples/02. Token configuration/01. Load session from database/client.sh
+++ examples/02. Token configuration/01. Load session from database/client.sh
@@ -1,10 +1,25 @@
-ACCESS_TOKEN="<paste_access_token_here>"
+LOGIN=Login-$RANDOM
 
-printf 'Check that session is valid\n'
-curl -X GET "http://localhost:9876/api/authorize?access-token=$ACCESS_TOKEN"
+printf 'Create new session\n'
+# In real world you will likely load previously received access-token from file
+curl -b .cookie -c .cookie --fail-with-body -X POST "http://localhost:9876/api/session" \
+  -H 'Content-Type: application/json' \
+  -d "{
+        \"entry\": \"app\",
+        \"logins\": {
+          \"username\": \"$LOGIN\"
+        },
+        \"signUp\": true,
+        \"authReqs\": {
+          \"password\": {}
+        }
+      }"
+
+printf '\n\nCheck that session is valid\n'
+curl -b .cookie -X POST "http://localhost:9876/api/authorize"
 
 printf '\n\nSign out\n'
-curl -X DELETE "http://localhost:9876/api/session?access-token=$ACCESS_TOKEN"
+curl -b .cookie -X DELETE "http://localhost:9876/api/session"
 
 printf '\n\nMust be expired after sign out\n'
-curl -X GET "http://localhost:9876/api/authorize?access-token=$ACCESS_TOKEN"
+curl -b .cookie -X POST "http://localhost:9876/api/authorize"
--- examples/02. Token configuration/04. Token rotation/client.sh
+++ examples/02. Token configuration/04. Token rotation/client.sh
@@ -1,6 +1,8 @@
-REFRESH_TOKEN="<paste_access_token_here>"
-
 printf 'Session can be refreshed manually\n'
+
+printf 'Enter refresh token: '
+read -r REFRESH_TOKEN
+
 curl -X POST "http://localhost:9876/api/token/refresh" \
   -H 'Content-Type: application/json' \
   -d "{ \"refreshToken\": \"$REFRESH_TOKEN\" }"
