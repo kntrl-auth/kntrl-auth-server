59bc565af7e24c07ed5dfd6d9a891ff43052b968
Dmitry Barannik <dmitryb.dev@gmail.com>
2022-10-26 00:13:59 +0300

    Facebook oauth example

--- examples/05. Social login/client.mjs
+++ examples/05. Social login/client.mjs
@@ -5,21 +5,21 @@ const prompt = Prompt({});
 
 async function example() {
   console.log('1. Create or open facebook app: https://developers.facebook.com/apps');
-  console.log('2. Edit kntrl.json5, put your app details there.');
+  console.log('2. Edit kntrl.json5, put your app details there. Restart kntrl app.');
 
-  console.log('3. Set appId (aka clientId) and redirectUri here');
-  const appId = '394838444474913';
-  const redirectUri = 'https://example.org';
+  console.log('3. Set appId (aka clientId) and redirectUri here. Don\'t forget, to add redirect-uri on developer page');
+  const appId = '<app-id>';
+  const redirectUri = 'https://oauthdebugger.com/debug';
 
-  console.log('4. Login to facebook by link: ' +
-    `https://www.facebook.com/v15.0/dialog/oauth?client_id=${appId}&redirect_uri=${redirectUri}`);
+  console.log('4. Log in to facebook by link: ' +
+    `https://www.facebook.com/v15.0/dialog/oauth?client_id=${appId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=openid`);
 
-  console.log('5. Set authorizationCode (code query param from url after facebook redirect)');
-  const authorizationCode = 'AQDxGgaStTvSYyWicC6-LWZHl8d3zT19oIoiOAVykpg5ahjwUOQigReIKoe8Hg8o7c5MSJIMwKC08LWsgAc1VExIIiwhHA-Jzq8E3B_w6CsK1Zn2SaTD2_piaCx6_pBV4w-3PcvN3LdiI80-hUmfyVElS5h4XZmazpE7CppsmgcQc6BSzeVBU18Vaf18qO5TOLkSFEGpoTHwU8sCb2woQyywVtSgdTerathK6aiJ1ZtndF-A3evuSOmINNQn8VO10AHz_Qu3_V33gjhrcTlhuMv9UkUVBzOAnAC-2BbHqq6Pdpl00WzicF95kTrGNhEiLGUUy-fnCtuVhQqNCmBOWGZRTh_z35wteUeTpXbvpQHr1l-ZZLq2Fc4NsLugZy1zkgo#_=_'
+  const authorizationCode = prompt("Enter authorization code: ");
 
   // Backend code
   const session = new Kntrl().newSession({
     entry: 'app',
+    // Sign in if account exists
     signIn: true,
     // Create new account if not exists
     signUp: true
@@ -35,6 +35,7 @@ async function example() {
   });
 
   console.log('Facebook auth result:', res.authRes.facebook);
+  console.log('New user:', res.user.new);
 }
 
 example().catch(err => console.log(err));
